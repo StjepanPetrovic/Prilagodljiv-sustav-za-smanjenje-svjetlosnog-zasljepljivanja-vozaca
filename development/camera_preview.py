6bfe057e152d47afde6f6742f130782bf9632625
Stjepan Petrovic <stjepanpetrovic11@gmail.com>
2023-08-15 00:00:56 +0200

    Draw info tekst on preview for exiting

--- development/camera_preview.py
+++ development/camera_preview.py
@@ -1,6 +1,10 @@
 import cv2
 
 
+def drawText(frame, txt, location, color=(50, 50, 170)):
+    cv2.putText(frame, txt, location, cv2.FONT_HERSHEY_SIMPLEX, 0.4, color, 1)
+
+
 def open_camera_preview():
     camera_index = 0
 
@@ -14,6 +18,8 @@ def open_camera_preview():
         has_frame, frame = source.read()
         if not has_frame:
             break
+
+        drawText(frame, 'Press ESC to exit', (20, 20))
 
         cv2.imshow(win_name, frame)
 
